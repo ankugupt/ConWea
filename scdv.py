04bf9c58459642a3321c304d8562f8fe8c830b96
Ankur Gupta <44271740+ankugupt@users.noreply.github.com>
2021-01-06 01:29:03 +0530

    Update scdv.py

--- scdv.py
+++ scdv.py
@@ -1,4 +1,358 @@
+from gensim.models import Word2Vec, FastText
+import pandas as pd
+import time
+import numpy as np
+import sys
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+from sklearn.mixture import GaussianMixture
+from sklearn.model_selection import GridSearchCV
+
 import pickle 
 with open('/content/df_contextualized10000end.pkl', 'rb') as f:
     word_cluster = pickle.load(f)
+with open('/content/df_contextualized10000end.pkl', 'rb') as f:
+    df = pickle.load(f)
+
+embedding = []
+key = []
+model={}
+for keys in word_cluster:
+  if len(word_cluster[keys])==1:
+    key.append(keys)
+    embedding.append(word_cluster[keys][0])
+    model[keys]=word_cluster[keys][0]
+  else:
+    for i in range(len(word_cluster[keys])):
+      key.append(keys+'$'+str(i))
+      embedding.append(word_cluster[keys][i])
+      model[keys+'$'+str(i)]=word_cluster[keys][i]
+      
+import pickle 
+
+with open('key.pkl', 'wb') as f:
+  pickle.dump(key, f)
+
+with open('embed.pkl', 'wb') as f:
+  pickle.dump(embedding, f)
+
+import pandas as pd
+df_train = df.truncate(before=0,after=2100)
+df_test = df.truncate(before=2101,after=2500)
+
+df.to_csv("all_v2.tsv",sep='\t')
+df_train.to_csv("train_v2.tsv",sep='\t')
+df_test.to_csv("test_v2.tsv",sep='\t')
+
+import re
+from nltk.corpus import stopwords
+def review_to_wordlist(review, remove_stopwords=False):
+        # Function to convert a document to a sequence of words,
+        # optionally removing stop words.  Returns a list of words.
+        # 1. Remove non-alphabets 
+        #review = re.sub("[^a-zA-Z]", " ", review)
+        # 2. Convert words to lower case and split them
+        words = review.split()
+        # 3. Optionally remove stop words (false by default)
+        if remove_stopwords:
+            stops = set(stopwords.words("english"))
+            words = [w for w in words if not w in stops and len(w) > 1]
+        # 4. Return a list of words
+        return (words)
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+def cluster_GMM(num_clusters, word_vectors):
+
+    idx = joblib.load("/content/gmm_latestclusmodel_len2alldata.pkl")
+    idx_proba = joblib.load("/content/gmm_prob_latestclusmodel_len2alldata.pkl")
+    return (idx,idx_proba)
+    # Initalize a GMM object and use it for clustering.
+    clf = GaussianMixture(n_components=num_clusters,
+                          covariance_type="tied", init_params='kmeans', max_iter=120,verbose=3)
+    # Get cluster assignments.
+    print(len(word_vectors))
+    clf.fit(word_vectors)
+    idx = clf.predict(word_vectors)
+    print("Clustering Done...", time.time() - start, "seconds")
+    # Get probabilities of cluster assignments.
+    idx_proba = clf.predict_proba(word_vectors)
+    # Dump cluster assignments and probability of cluster assignments. 
+    joblib.dump(idx, 'gmm_latestclusmodel_len2alldata.pkl')
+    print("Cluster Assignments Saved...")
+
+    joblib.dump(idx_proba, 'gmm_prob_latestclusmodel_len2alldata.pkl')
+    print("Probabilities of Cluster Assignments Saved...")
+    return (idx, idx_proba)
+
+def read_GMM(idx_name, idx_proba_name):
+    # Loads cluster assignments and probability of cluster assignments. 
+    idx = joblib.load(idx_name)
+    idx_proba = joblib.load(idx_proba_name)
+    print("Cluster Model Loaded...")
+    return (idx, idx_proba)
+
+
+def get_probability_word_vectors(featurenames, word_centroid_map, num_clusters, word_idf_dict):
+    # This function computes probability word-cluster vectors.
+
+    prob_wordvecs = {}
+    count = 0 
+    exp=0
+
+    for word in word_centroid_map:
+        prob_wordvecs[word] = np.zeros(num_clusters * num_features, dtype="float32")
+        for index in range(0, num_clusters):
+            #prob_wordvecs[word][index * num_features:(index + 1) * num_features] = model[word] * word_centroid_prob_map[word][index] * word_idf_dict[word]
+            count+=1
+            try:
+                prob_wordvecs[word][index * num_features:(index + 1) * num_features] = model[word] * word_centroid_prob_map[word][index] * word_idf_dict[word]
+                #print(word)
+            except:
+                exp+=1
+                continue
+    print("hello"+str(exp/count))
+
+    # prob_wordvecs_idf_len2alldata = {}
+    # i = 0
+    # for word in featurenames:
+    # 	i += 1
+    # 	if word in word_centroid_map:	
+    # 		prob_wordvecs_idf_len2alldata[word] = {}
+    # 		for index in range(0, num_clusters):
+    # 				prob_wordvecs_idf_len2alldata[word][index] = model[word] * word_centroid_prob_map[word][index] * word_idf_dict[word] 
+
+    # for word in prob_wordvecs_idf_len2alldata.keys():
+    # 	prob_wordvecs[word] = prob_wordvecs_idf_len2alldata[word][0]
+    # 	for index in prob_wordvecs_idf_len2alldata[word].keys():
+    # 		if index==0:
+    # 			continue
+    # 		prob_wordvecs[word] = np.concatenate((prob_wordvecs[word], prob_wordvecs_idf_len2alldata[word][index]))
+    return prob_wordvecs
+
+def create_cluster_vector_and_gwbowv(prob_wordvecs, wordlist, word_centroid_map, word_centroid_prob_map, dimension,
+                                     word_idf_dict, featurenames, num_centroids, train=False):
+    # This function computes SDV feature vectors.
+    bag_of_centroids = np.zeros(num_centroids * dimension, dtype="float32")
+    global min_no
+    global max_no
+    count = 0 
+    exp_counter = 0
+
+    for word in wordlist:
+        count+=1
+        try:
+            temp = word_centroid_map[word]
+        except:
+            exp_counter+=1
+            continue
+
+        bag_of_centroids += prob_wordvecs[word]
+    #print(exp_counter/count)
+  
+
+    norm = np.sqrt(np.einsum('...i,...i', bag_of_centroids, bag_of_centroids))
+    if (norm != 0):
+        bag_of_centroids /= norm
+
+    # To make feature vector sparse, make note of minimum and maximum values.
+    if train:
+        min_no += min(bag_of_centroids)
+        max_no += max(bag_of_centroids)
+
+    return bag_of_centroids
+
+if __name__ == '__main__':
+
+    start = time.time()
+
+    num_features =  768  # Word vector dimensionality
+    min_word_count = 40  # Minimum word count
+    num_workers = 40  # Number of threads to run in parallel
+    context = 10  # Context window size
+    downsampling = 1e-3  # Downsample setting for frequent words
+
+    #model_type = sys.argv[3]
+
+    model_name = str(num_features) + "features_" + str(min_word_count) + "minwords_" + str(
+        context) + "context_len2alldata"
+
+    # Load train data.
+    train = pd.read_csv('/content/train_v2.tsv', header=0, delimiter="\t")
+    # Load test data.
+    test = pd.read_csv('/content/test_v2.tsv', header=0, delimiter="\t")
+    all = pd.read_csv('/content/all_v2.tsv', header=0, delimiter="\t")
+    all= all.truncate(before=0,after=2500)
+
+    import pickle 
+    with open('/content/key.pkl', 'rb') as f:
+      index2word = pickle.load(f)
+    with open('/content/embed.pkl', 'rb') as f:
+      word_vectors = pickle.load(f)
+
+    for idx, vec in enumerate(word_vectors):
+      if len(vec)!=768:
+        word_vectors[idx]=word_vectors[24718]
     
+
+    # Set number of clusters.
+    num_clusters = 60
+    # Uncomment below line for creating new clusters.
+    #idx, idx_proba = cluster_GMM(num_clusters, word_vectors)
+
+    # Uncomment below lines for loading saved cluster assignments and probabaility of cluster assignments.
+    idx_name = "gmm_latestclusmodel_len2alldata.pkl"
+    idx_proba_name = "gmm_prob_latestclusmodel_len2alldata.pkl"
+    idx, idx_proba = read_GMM(idx_name, idx_proba_name)
+
+    # Create a Word / Index dictionary, mapping each vocabulary word to
+    # a cluster number
+    word_centroid_map = dict(zip(index2word, idx))
+    # Create a Word / Probability of cluster assignment dictionary, mapping each vocabulary word to
+    # list of probabilities of cluster assignments.
+    word_centroid_prob_map = dict(zip(index2word, idx_proba))
+
+    # Computing tf-idf values.
+    traindata = []
+    for i in range(0, len(all["news"])):
+        traindata.append(" ".join(review_to_wordlist(all["news"][i], True)))
+
+    tfv = TfidfVectorizer(strip_accents='unicode', dtype=np.float32)
+    tfidfmatrix_traindata = tfv.fit_transform(traindata)
+    featurenames = tfv.get_feature_names()
+    idf = tfv._tfidf.idf_
+
+    def calculate_df_doc_freq(df):
+      docfreq = {}
+      docfreq["UNK"] = len(df)
+      for index, row in df.iterrows():
+          line = row["news"]
+          words = line.strip().split()
+          temp_set = words
+          for w in temp_set:
+              try:
+                  docfreq[w] += 1
+              except:
+                  docfreq[w] = 1
+      return docfreq
+
+    def calculate_inv_doc_freq(df, docfreq):
+      inv_docfreq = {}
+      N = len(df)
+      print(N)
+      for word in docfreq:
+          inv_docfreq[word] = np.log(N / docfreq[word])
+      return inv_docfreq
+
+    # Creating a dictionary with word mapped to its idf value 
+    print("Creating word-idf dictionary for Training set...")
+
+    word_idf_dict = {}
+
+    '''
+    for pair in zip(featurenames, idf):
+        word_idf_dict[pair[0]] = pair[1]
+    '''
+
+    word_idf_dict = calculate_inv_doc_freq(df,calculate_df_doc_freq(all))
+
+
+    # Pre-computing probability word-cluster vectors.
+    prob_wordvecs = get_probability_word_vectors(featurenames, word_centroid_map, num_clusters, word_idf_dict)
+
+    temp_time = time.time() - start
+    print("Creating Document Vectors...:", temp_time, "seconds.")
+
+    # gwbowv is a matrix which contains normalised document vectors.
+    gwbowv = np.zeros((train["news"].size, num_clusters * (num_features)), dtype="float32")
+
+    counter = 0
+
+    min_no = 0
+    max_no = 0
+    for review in train["news"]:
+        # Get the wordlist in each news article.
+        words = review_to_wordlist(review, remove_stopwords=True)
+        gwbowv[counter] = create_cluster_vector_and_gwbowv(prob_wordvecs, words, word_centroid_map,
+                                                           word_centroid_prob_map, num_features, word_idf_dict,
+                                                           featurenames, num_clusters, train=True)
+        counter += 1
+        if counter % 100 == 0:
+            print("Train News Covered : ", counter)
+
+    gwbowv_name = "SDV_" + str(num_clusters) + "cluster_" + str(num_features) + "feature_matrix_gmm_sparse.npy"
+
+    gwbowv_test = np.zeros((test["news"].size, num_clusters * (num_features)), dtype="float32")
+
+    counter = 0
+
+    for review in test["news"]:
+        # Get the wordlist in each news article.
+        words = review_to_wordlist(review, \
+                                                         remove_stopwords=True)
+        gwbowv_test[counter] = create_cluster_vector_and_gwbowv(prob_wordvecs, words, word_centroid_map,
+                                                                word_centroid_prob_map, num_features, word_idf_dict,
+                                                                featurenames, num_clusters)
+        counter += 1
+        if counter % 1000 == 0:
+            print("Test News Covered : ", counter)
+
+    test_gwbowv_name = "TEST_SDV_" + str(num_clusters) + "cluster_" + str(
+        num_features) + "feature_matrix_gmm_sparse.npy"
+
+    print("Making sparse...")
+    # Set the threshold percentage for making it sparse. 
+    percentage = 0.04
+    min_no = min_no * 1.0 / len(train["news"])
+    max_no = max_no * 1.0 / len(train["news"])
+    print("Average min: ", min_no)
+    print("Average max: ", max_no)
+    thres = (abs(max_no) + abs(min_no)) / 2
+    thres = thres * percentage
+
+    #Make values of matrices which are less than threshold to zero.
+    #temp = abs(gwbowv) < thres
+    #gwbowv[temp] = 0
+
+    #temp = abs(gwbowv_test) < thres
+    #gwbowv_test[temp] = 0
+
+    # saving gwbowv train and test matrices
+    np.save(gwbowv_name, gwbowv)
+    np.save(test_gwbowv_name, gwbowv_test)
+
+    endtime = time.time() - start
+    print("SDV created and dumped: ", endtime, "seconds.")
+    print("Fitting a SVM classifier on labeled training data...")
+
+    param_grid = [
+        {'C': np.arange(5.25,5.5,0.25)}]
+    scores = ['accuracy', 'recall_micro', 'f1_micro', 'precision_micro', 'recall_macro', 'f1_macro', 'precision_macro',
+              'recall_weighted', 'f1_weighted', 'precision_weighted']  # , 'accuracy', 'recall', 'f1']
+    for score in scores:
+        strt = time.time()
+        print("# Tuning hyper-parameters for", score, "\n")
+        clf = GridSearchCV(LinearSVC(C=1), param_grid, cv=2, scoring='%s' % score,verbose=3)
+        clf.fit(gwbowv, train["class"])
+        print("Best parameters set found on development set:\n")
+        print(clf.best_params_)
+        print("Best value for ", score, ":\n")
+        file1 = open("report.txt","w+")
+        file1.write("Best value for ", score, ":\n"+ str(clf.best_score_)+"para"+str(clf.best_params_))
+        print(clf.best_score_)
+        Y_true, Y_pred = test["class"], clf.predict(gwbowv_test)
+        print("Report")
+        print(classification_report(Y_true, Y_pred, digits=6))
+        print("Accuracy: ", clf.score(gwbowv_test, test["class"]))
+        file1.write("TEST ACCURACY"+str(clf.score(gwbowv_test, test["class"])))
+        print("Time taken:", time.time() - strt, "\n")
+    endtime = time.time()
+    print("Total time taken: ", endtime - start, "seconds.")
+
+    print("********************************************************")
